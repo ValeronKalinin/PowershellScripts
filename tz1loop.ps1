72c21316d103ab1b3e173a61cea3d9f42d19465f
VK <rokes@list.ru>
2020-02-27 15:19:38 +0300

    add file tz1loop

---
+++ tz1loop.ps1
@@ -0,0 +1,12 @@
+﻿$data=Read-Host 
+$i=0;$check=0
+
+while(($i -lt $data.Length) -and ($check -ge 0))
+{
+if ( $data[$i] -eq "("){$check++}
+else {$check-- }
+$i++
+}
+
+if ($check -eq 0){Write-Host "$data is ok" }
+else {Write-Host "$data is not ok in element: " $i  }
