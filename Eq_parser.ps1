8ae84d805a8f5c748be5ae99d725a12c05891c77
ValeronKalinin <36825149+ValeronKalinin@users.noreply.github.com>
2020-03-21 20:59:42 +0300

    Update Eq_parser.ps1

--- Eq_parser.ps1
+++ Eq_parser.ps1
@@ -1,43 +1,48 @@
-#Запуск секундомера
+#Р—Р°РїСѓСЃРє СЃРµРєСѓРЅРґРѕРјРµСЂР°
 $Timer = [System.Diagnostics.Stopwatch]::StartNew()
 $Timer1 = [System.Diagnostics.Stopwatch]::StartNew()
 $Timer.Start()
 
+
 #$tempObj = New-Object psobject
-#Хэштаблица для хранения  пар TRAN_ID - время
+#РҐСЌС€С‚Р°Р±Р»РёС†Р° РґР»СЏ С…СЂР°РЅРµРЅРёСЏ  РїР°СЂ TRAN_ID - РІСЂРµРјСЏ
 $table = @{ } 
-#Храним обработанные данные по каждой секунде
+$tablelong = @{ } 
+#РҐСЂР°РЅРёРј РѕР±СЂР°Р±РѕС‚Р°РЅРЅС‹Рµ РґР°РЅРЅС‹Рµ РїРѕ РєР°Р¶РґРѕР№ СЃРµРєСѓРЅРґРµ
 $result = @{ }
-#Усредненные данные по каждой секунде
+#РЈСЃСЂРµРґРЅРµРЅРЅС‹Рµ РґР°РЅРЅС‹Рµ РїРѕ РєР°Р¶РґРѕР№ СЃРµРєСѓРЅРґРµ
 $Data = @()
-#переменная для хранения данных таймера
+#РїРµСЂРµРјРµРЅРЅР°СЏ РґР»СЏ С…СЂР°РЅРµРЅРёСЏ РґР°РЅРЅС‹С… С‚Р°Р№РјРµСЂР°
 $seconds = $null
 
-# Файл  лога который будем парсить
+# Р¤Р°Р№Р»  Р»РѕРіР° РєРѕС‚РѕСЂС‹Р№ Р±СѓРґРµРј РїР°СЂСЃРёС‚СЊ
 $Path_in="C:\Users\vk\Desktop\eq\ineq.txt"
-#Файл вывода куда будем писать обработанные данные
+#Р¤Р°Р№Р» РІС‹РІРѕРґР° РєСѓРґР° Р±СѓРґРµРј РїРёСЃР°С‚СЊ РѕР±СЂР°Р±РѕС‚Р°РЅРЅС‹Рµ РґР°РЅРЅС‹Рµ
 $Path_out="C:\Users\vk\Desktop\eq\inEQ.out"
 
 $reader = [System.IO.File]::OpenText($Path_in)
 while (!($reader.EndOfStream)) {
    
-    #Читаем строку
+    #Р§РёС‚Р°РµРј СЃС‚СЂРѕРєСѓ
     $line = $reader.ReadLine()
-
+        
         $direction =    $line.split(":")[2].Split(";")[-1]
         $time = $line.split(";")[0]
         $TRAN_ID = $line.split(" ")[2].split(";")[0]
         $seconds_now = $time.split(".")[0].split(":")[2] 
         
-        # Когда кончается секунда считаем данные (среднее, медиану, 99 перцентиль, максимум, количество)
+        # РљРѕРіРґР° РєРѕРЅС‡Р°РµС‚СЃСЏ СЃРµРєСѓРЅРґР° СЃС‡РёС‚Р°РµРј РґР°РЅРЅС‹Рµ (СЃСЂРµРґРЅРµРµ, РјРµРґРёР°РЅСѓ, 99 РїРµСЂС†РµРЅС‚РёР»СЊ, РјР°РєСЃРёРјСѓРј, РєРѕР»РёС‡РµСЃС‚РІРѕ)
         
     if ($seconds -ne $seconds_now -and $seconds -ne $null) {
-        
-        
-        
-        $tempObj=@{}
+         
+        $Timer3.Stop()
+        write-host $Timer3.Elapsed
+        #$Timer2 = [System.Diagnostics.Stopwatch]::StartNew()
+        #$Timer2.Start()  
+        <##>
+        $tempObj = @{}
         $summ = $null 
-        # Максимальная задержка в МИКРОСЕКУНДАХ
+        # РњР°РєСЃРёРјР°Р»СЊРЅР°СЏ Р·Р°РґРµСЂР¶РєР° РІ РњРРљР РћРЎР•РљРЈРќР”РђРҐ
         $max = 0 
         foreach ($r in $result.Values) {
                 
@@ -45,75 +50,74 @@ while (!($reader.EndOfStream)) {
                 
             $summ += $r 
         } 
-        # Количество
+        # РљРѕР»РёС‡РµСЃС‚РІРѕ
         $count = $result.Count 
-        # Средняя задержка в МИКРОСЕКУНДАХ                      
+        # РЎСЂРµРґРЅСЏСЏ Р·Р°РґРµСЂР¶РєР° РІ РњРРљР РћРЎР•РљРЈРќР”РђРҐ                      
         $average = [math]::Round(($summ / $count), 2)    
-        # Считаем медиану
-        $Mediandata = $result.Values | sort
+        # РЎС‡РёС‚Р°РµРј РјРµРґРёР°РЅСѓ
+        $Mediandata =  Sort-Object -InputObject $result.Values 
         $MedianValue = $Mediandata[[math]::Round($count / 100 * 50)]
 
-        # 99 процентиль
+        # 99 РїСЂРѕС†РµРЅС‚РёР»СЊ
         $P99 = $null
         $P99 = $Mediandata[[math]::Round($count / 100 * 99)]
             
-        # Собираем строку отчет�
+        # РЎРѕР±РёСЂР°РµРј СЃС‚СЂРѕРєСѓ РѕС‚С‡РµС‚пїЅ
         
-         Add-Member -InputObject $tempObj -MemberType NoteProperty -name  "Time" -Value $line.split(";")[0].split(".")[0] -Force
-         Add-Member -InputObject $tempObj -MemberType NoteProperty -name  "Average" -Value $average  -Force
-         Add-Member -InputObject $tempObj -MemberType NoteProperty -name  "Median" -Value $MedianValue -Force
-         Add-Member -InputObject $tempObj -MemberType NoteProperty -name  "P99" -Value $P99 -Force
-         Add-Member -InputObject $tempObj -MemberType NoteProperty -name  "Count" -Value $count -Force
-         Add-Member -InputObject $tempObj -MemberType NoteProperty -name  "Max" -Value $max  -Force
-         
-        # сохраняем строку отчета
-        $Data += $tempObj 
+        $tempObj["Time"]=$line.split(";")[0].split(".")[0]
+        $tempObj["Average"]=$average 
+        $tempObj["Median"]=$MedianValue 
+        $tempObj["P99"]=$P99
+        $tempObj["Count"]=$count
+        $tempObj["Max"]=$max
 
+        $obj=New-Object -TypeName psobject -Property $tempObj
+        # СЃРѕС…СЂР°РЅСЏРµРј СЃС‚СЂРѕРєСѓ РѕС‚С‡РµС‚Р°
+        $Data += $obj
         $result = @{ }
-        
-        $Timer2.Stop()
-        write-host $Timer2.Elapsed  
-        # Таймер для анализа скорости обработки 1 секунды лога
-        $Timer2 = [System.Diagnostics.Stopwatch]::StartNew()
-        $Timer2.Start()   
+
+        # РўР°Р№РјРµСЂ РґР»СЏ Р°РЅР°Р»РёР·Р° СЃРєРѕСЂРѕСЃС‚Рё РѕР±СЂР°Р±РѕС‚РєРё 1 СЃРµРєСѓРЅРґС‹ Р»РѕРіР°
+        #$Timer2.Stop()
+        #write-host $Timer2.Elapsed 
+
+        $Timer3 = [System.Diagnostics.Stopwatch]::StartNew()
+        $Timer3.Start() 
     }
        
     
-    # В этой секции я пишу в Хештаблицу данные если нашел строку INPUT
+    # Р’ СЌС‚РѕР№ СЃРµРєС†РёРё СЏ РїРёС€Сѓ РІ РҐРµС€С‚Р°Р±Р»РёС†Сѓ РґР°РЅРЅС‹Рµ РµСЃР»Рё РЅР°С€РµР» СЃС‚СЂРѕРєСѓ INPUT
     if ( $direction -eq "INPUT") {
         
-        $table[$TRAN_ID] = $time      
+        $table[$TRAN_ID] = $time
+       
     }
     
    
-    if ($direction -eq "OUTPUT" -and $table[$TRAN_ID]) {    
-        
+    if ($direction -eq "OUTPUT" -and ($tstart=$table[$TRAN_ID])) {    
 
-        $tstart = $table[$TRAN_ID]
+        #$tstart = $table[$TRAN_ID]
 
         #if ($tstart -ne $null) {
             
             $tfinish = $time 
                 
-            #  Считаем задержку
+            #  РЎС‡РёС‚Р°РµРј Р·Р°РґРµСЂР¶РєСѓ
             $latency = [math]::Round([math]::Abs(  ([timespan]$tstart).TotalMilliseconds - ([timespan]$tfinish).TotalMilliseconds), 3) * 1000
                 
-            # Пишем в таблицу задержку в конкретный момент времени (время OUTPUT)
+            # РџРёС€РµРј РІ С‚Р°Р±Р»РёС†Сѓ Р·Р°РґРµСЂР¶РєСѓ РІ РєРѕРЅРєСЂРµС‚РЅС‹Р№ РјРѕРјРµРЅС‚ РІСЂРµРјРµРЅРё (РІСЂРµРјСЏ OUTPUT)
             $result[$tstart] = $latency
                 
-            # Раскоментировать для проверки глазами 
+            # Р Р°СЃРєРѕРјРµРЅС‚РёСЂРѕРІР°С‚СЊ РґР»СЏ РїСЂРѕРІРµСЂРєРё РіР»Р°Р·Р°РјРё 
             #write-host $testID " " $table.($TRAN_ID.trim()) " "  $time 
                 
-            #Удаляю из Хештаблицы запись
-            if($maxtablecount -le $table.Count) {$maxtablecount=$table.Count}
-
+            #РЈРґР°Р»СЏСЋ РёР· РҐРµС€С‚Р°Р±Р»РёС†С‹ Р·Р°РїРёСЃСЊ
             $table.Remove($TRAN_ID)
     
     }
-     
+
     $seconds = $seconds_now
-    
-#}   
+     
 }
 $Timer.Stop()
 write-host $Timer.Elapsed $Data.count $maxtablecount
+$Data | Export-Csv -Path $Path_out -Encoding UTF8 -NoTypeInformation  
